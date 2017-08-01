c2bef628262b8122ad07b5db03348a89c7f5b878
Ivan Kuzmin <inkuzm@gmail.com>
2017-08-01 18:44:47 +0300

    implement aea

--- lib/aea.ex
+++ lib/aea.ex
@@ -1,18 +1,85 @@
 defmodule AEA do
-  @moduledoc """
-  Documentation for AEA.
-  """
 
-  @doc """
-  Hello world.
+  def init(genes, term, iterations) do
 
-  ## Examples
+    {m_gt, m_g, m_t} = determine_ms(genes, term)
 
-      iex> AEA.hello
-      :world
+    gs = AEA.Helpers.get_ets_keys_lazy(:genes_to_terms) |> Enum.to_list
+    ts = AEA.Helpers.get_ets_keys_lazy(:terms_to_genes) |> Enum.to_list
 
-  """
-  def hello do
-    :world
+    mtilda_gts = multiple_randomize(gs, ts, m_g, m_t, iterations, [])
+
+    p = ((Enum.filter mtilda_gts, fn(mtilda_gt) -> mtilda_gt >= m_gt  end) |> length) / length(mtilda_gts)
+
+    {m_gt, mtilda_gts, p}
   end
+  def init(genes, term) do
+    init(genes, term, 10_000)
+  end
+
+
+  def determine_ms(genes, term) do
+      terms = term |> get_terms_of_branch
+
+      m_gt = determine_m_gt genes, terms
+      m_t = terms |> get_genes_for_terms |> List.flatten |> length
+      m_g = genes |> get_terms_for_genes |> List.flatten |> length
+
+      {m_gt, m_g, m_t}
+  end
+
+  def shuffle(:genes) do
+    AEA.Helpers.get_ets_keys_lazy(:genes_to_terms) |> Enum.to_list |> Enum.shuffle
+  end
+  def shuffle(:terms) do
+    AEA.Helpers.get_ets_keys_lazy(:terms_to_genes) |> Enum.to_list |> Enum.shuffle
+  end
+
+  def randomize(gs, ts, m_g, m_t) do
+    shuffled_genes = gs |> Enum.shuffle
+    shuffled_terms = ts |> Enum.shuffle
+
+    genes = AEA.Helpers.get_keys_with_closest_number_of_values :genes_to_terms, m_g, shuffled_genes, 0, []
+    terms = AEA.Helpers.get_keys_with_closest_number_of_values :terms_to_genes, m_t, shuffled_terms, 0, []
+
+    {genes, terms}
+  end
+
+  def multiple_randomize(gs, ts, m_g, m_t, 0, mtilda_gts) do
+      mtilda_gts
+  end
+  def multiple_randomize(gs, ts, m_g, m_t, n, mtilda_gts) when n > 0 do
+      {g, t} = randomize(gs, ts, m_g, m_t)
+      mtilda_gt = determine_m_gt(g, t)
+      multiple_randomize(gs, ts, m_g, m_t, n - 1, [mtilda_gt | mtilda_gts])
+  end
+
+  def determine_m_gt(genes, terms) do
+    gene_sets = terms |> get_genes_for_terms
+
+    m_gt = Enum.reduce gene_sets, 0, fn (gene_set, acc) ->
+      acc + (AEA.Helpers.intersect(gene_set, genes) |> length)
+    end
+  end
+
+
+  def get_terms_for_genes(genes) do
+    AEA.Helpers.get_values_for_keys(:genes_to_terms, genes)
+  end
+
+  def get_genes_for_terms(terms) do
+    AEA.Helpers.get_values_for_keys(:terms_to_genes, terms)
+  end
+
+
+  def get_terms_of_branch(term) do
+        case :ets.lookup(:terms_to_terms, term) do
+            [{ term, terms }] ->
+                [ term | terms ]
+            _ ->
+                IO.puts "Parsing error with #{inspect term}"
+
+        end
+  end
+
 end
--- lib/helpers.ex
+++ lib/helpers.ex
@@ -29,4 +29,52 @@ defmodule AEA.Helpers do
     def intersect(a, b), do: a -- (a -- b)
 
 
+     def get_values_for_keys(table, keys) when is_atom(table) do
+       Enum.map keys, fn (k) ->
+         case :ets.lookup(table, k) do
+             [{_, values}] ->
+                 values
+             [] ->
+                 []
+             _ ->
+                 IO.puts "Another error with #{inspect k}"
+         end
+       end
+     end
+
+
+    @doc """
+    One of the most important functions.
+    """
+    def get_keys_with_closest_number_of_values(table, required_number_of_values, [ key ], acc_number_of_values, acc_keys)  when is_atom(table) do
+      if acc_number_of_values == 0 do
+          # This guarantees that at list one key will be returned
+          [ key ]
+      else
+          acc_keys
+      end
+    end
+    def get_keys_with_closest_number_of_values(table, required_number_of_values, [key | keys], acc_number_of_values, acc_keys)  when is_atom(table) do
+      i = case :ets.lookup(table, key) do
+          [{_, values}] ->
+            length(values)
+          [] ->
+            0
+          _ ->
+            IO.puts "Error with #{inspect key}"
+            0
+      end
+
+      if (acc_number_of_values + i) - required_number_of_values >= 0 do
+          if abs((acc_number_of_values + i) - required_number_of_values) <= abs(acc_number_of_values - required_number_of_values) do
+              get_keys_with_closest_number_of_values(table, required_number_of_values, [key], (acc_number_of_values + i), [key | acc_keys])
+          else
+              get_keys_with_closest_number_of_values(table, required_number_of_values, [key], acc_number_of_values, acc_keys)
+          end
+      else
+          get_keys_with_closest_number_of_values(table, required_number_of_values, keys, (acc_number_of_values + i), [key | acc_keys])
+      end
+    end
+
+
 end
